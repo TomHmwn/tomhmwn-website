0cbcbb0f4eeebd4fb321d8c45cc2a43b1c61d879
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-08-11 02:13:22 +1000

    update my resume

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'reactcd'
 import ContainerBlock from "../components/ContainerBlock";
 import AboutMe from "../components/AboutMe";
 const about = () => {
